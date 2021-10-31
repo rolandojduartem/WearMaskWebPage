16f82dd259391c59340877ffa9b1a1d18f9fcd90
rolandojduartem <rolando.duartemejias@gmail.com>
2021-10-31 06:14:01 -0600

    Fixxing Camera: Client webcam

--- app.py
+++ app.py
@@ -1,23 +1,26 @@
 import os
 import logging
+from io import BytesIO
+from PIL import Image
 
 #Prevents warnings
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'  # FATAL
 logging.getLogger('tensorflow').setLevel(logging.FATAL)
 
+from flask_socketio import SocketIO, emit
+
 import cv2
+import base64
 import numpy as np
 from time import sleep
 import tensorflow as tf
-from threading import Thread
-from turbo_flask import Turbo
 import tensorflow.keras as keras
-from flask import Flask, Response, make_response, render_template
+from flask import Flask, make_response, render_template
 
 #Initializing variables
 app = Flask(__name__)
-turbo = Turbo(app)
-camera = cv2.VideoCapture(-1)
+socketio = SocketIO(app, cors_allowed_origins="*")
+
 frontalFaceCascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
 model =  keras.models.load_model("./WearMask/Model/bestModel.h5")
 
@@ -30,13 +33,12 @@ def predict(frame):
     frame = tf.image.rgb_to_grayscale(frame)
     y_prob = np.around(model.predict(frame)[0] * 100, 2)
     return y_prob[0]
-
+"""
 @app.before_first_request
 def beforeFirstRequest():
     Thread(target=updateLoad).start()
 
 def updateLoad():
-    """ Refreshing probabilities on the app """
     with app.app_context():
         while True:
             sleep(1)
@@ -44,9 +46,8 @@ def updateLoad():
 
 @app.context_processor            
 def genModel():
-    """ Model application functionality """
     try:
-        success, frame = camera.read()  # read the camera frame
+        frame= request.files.get('video')
         frame = cv2.flip(frame,1)
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         frontalFaces = frontalFaceCascade.detectMultiScale(
@@ -66,35 +67,56 @@ def genModel():
     except:
         y_prob = -1
     return {"y_prob": y_prob}   
-    
-def genFrames():
+"""
+@socketio.on("image")
+def genFrames(imageData):
     """ Video stream functionality """
-    while True:
-        try:
-            success, frame = camera.read()  # read the camera frame
-            frame = cv2.flip(frame,1)
-            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-            frontalFaces = frontalFaceCascade.detectMultiScale(
-                        gray,
-                        scaleFactor=1.1,
-                        minNeighbors=5,
-                        minSize=(128, 128),
-                        flags = cv2.CASCADE_SCALE_IMAGE
-            )
+    sleep(0.1)
+    try:
+        idx = imageData.find('base64,')
+        base64Data  = imageData[idx+7:]
+        decoder = BytesIO()
+        decoder.write(base64.b64decode(base64Data, ' /'))
+        image = Image.open(decoder)
+        frame = np.array(image)
+        frame = cv2.flip(frame, 1)
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        frontalFaces = frontalFaceCascade.detectMultiScale(
+                    gray,
+                    scaleFactor=1.1,
+                    minNeighbors=5,
+                    minSize=(128, 128),
+                    flags = cv2.CASCADE_SCALE_IMAGE
+        )
 
-            if not success:
-                camera = cv2.VideoCapture(-1)
-            else:
-                for (x, y, w, h) in frontalFaces:
-                    cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
-                #    frame = frame[y:y + h, x + 10:x + w - 10, :]
-                ret, buffer = cv2.imencode('.jpg', frame)
-                frame = buffer.tobytes()
-                yield (b'--frame\r\n'
-                        b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
-        except:
-            pass
+        for (x, y, w, h) in frontalFaces:
+            cv2.rectangle(gray, (x, y), (x+w, y+h), (0, 255, 0), 2)
+            frame = frame[y - 25:y + h + 60, x +20 :x + w - 20, :]
+            y_prob = predict(frame)
+        ret, buffer = cv2.imencode('.jpeg', gray)
+        stringData = base64.b64encode(buffer).decode('utf-8')
+        b64Head = 'data:image/jpeg;base64,'
+        stringData = b64Head + stringData
 
+        if y_prob >=50:
+            print(y_prob)
+            emit('response_back', {
+                "image": stringData, 
+                "condition": "Mask", 
+                "probability": str(y_prob)
+            })
+        else:
+            emit('response_back', {
+                "image": stringData, 
+                "condition": "No Mask", 
+                "probability": str(y_prob)
+            })
+    except:
+        emit('response_back', {
+                "image": stringData, 
+                "condition": "Wait a moment", 
+                "probability": "Calculating..."
+            })
 
 @app.route("/")
 def index():
@@ -104,9 +126,5 @@ def index():
 def appPage():
     return make_response(render_template("app.html"))
 
-@app.route('/video_feed')
-def video_feed():
-    return Response(genFrames(), mimetype='multipart/x-mixed-replace; boundary=frame')
-
 if __name__ == "__main__":
-    app.run()
+    socketio.run(app, debug=True)
